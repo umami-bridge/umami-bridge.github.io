0b54a86566992c3e45e11083309317f40df37698
x <x@MacBook-Pro-3.lan>
2022-04-06 11:22:59 -0700

    Updates

---
+++ _next/static/jChydJl7QXpcSdiCKj9EL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/77-690d1c5b6df396b1.js","static/css/ea8345fbbb84e9a9.css","static/chunks/pages/index-54c91f5ace054eab.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
