3690fb2f3142b4eadcba279d5794ef4b8a6afc0f
x <x@MacBook-Pro-3.lan>
2022-04-06 11:57:48 -0700

    update build script

--- buildSetup.sh
+++ buildSetup.sh
@@ -1,4 +1,4 @@
 cp -rf build/_next/static build/static
-sed 's/\/_next/./' build/index.html > build/temp.html
+sed 's/\/_next/./g' build/index.html > build/temp.html
 rm -rf build/index.html
 mv build/temp.html build/index.html
